98b665a2b601fdb45b5e01303f17839c53104338
Bin Zhang <bin@xnatworks.io>
2026-01-26 21:24:31 -0600

    Upgrade from dcm4che5 5.31.0,javax, to 5.33.1-jakata

--- src/main/java/org/nrg/xnatx/dicomweb/service/wado/WadoOutputWriters.java
+++ src/main/java/org/nrg/xnatx/dicomweb/service/wado/WadoOutputWriters.java
@@ -51,8 +51,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.json.Json;
-import javax.json.stream.JsonGenerator;
+import jakarta.json.Json;
+import jakarta.json.stream.JsonGenerator;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.*;
